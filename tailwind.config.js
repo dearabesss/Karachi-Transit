7d2fd73dcb21d2bd219d823062e9aff42868a62a
dearabess <128939887+dearabesss@users.noreply.github.com>
2026-08-18 01:08:51 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // Catches standard root directories
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./data/**/*.{js,ts,jsx,tsx,mdx}",
-    
-    // Catches everything if you used the 'src' directory during setup
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -16,7 +12,7 @@ module.exports = {
           green: "#047857",
           red: "#dc2626",
           ev: "#0284c7",
-          orange: "#ea580c"
+          pink: "#db2777"
         }
       }
     },

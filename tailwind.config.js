c47ab640a647c9add21df2fb12ee222c41f927a1
dearabess <128939887+dearabesss@users.noreply.github.com>
2026-08-17 18:23:05 +0500

    Update and rename talwind.config.js to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    // Next.js App Directory
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    // Components Directory
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    // If you placed data files that include Tailwind class strings (like our badge colors)
+    "./data/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
